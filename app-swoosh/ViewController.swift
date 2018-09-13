eb37db1846a797d07a1c78d10eaaaf3f29e6bb9e
Andy Taylor <Theador0691@gmail.com>
2018-09-13 21:52:00 +0400

    Initial Commit

---
+++ app-swoosh/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  app-swoosh
+//
+//  Created by Andrew Taylor on 13/09/2018.
+//  Copyright © 2018 Andrew Taylor. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+

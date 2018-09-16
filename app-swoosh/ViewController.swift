836297a36e569fa0d138ff438fdcbbe5804d7c9a
Andy Taylor <Theador0691@gmail.com>
2018-09-16 22:49:55 +0400

    Adjusted the auto layout

--- app-swoosh/ViewController.swift
+++ app-swoosh/ViewController.swift
@@ -10,9 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var backgroundImage: UIImageView!
+    @IBOutlet weak var swoosh: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
 
